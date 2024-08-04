8ebe1783c265cd9c5d5ebe42063266fecf045399
Aripov-Sirojiddin <aripov.2@osu.edu>
2024-08-04 12:35:12 -0400

    Validating user model with more tests and contraints

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  before_save {self.email = self.email.downcase}
+  validates :name,
+            presence: true,
+            length: {
+              minimum: 1,
+              maximum: 50
+            }
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            length: {
+              maximum: 255
+            },
+            format: {
+              with: URI::MailTo::EMAIL_REGEXP
+            }
 end
