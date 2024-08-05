18d0251e283fd346f27f853ac971a431cc9d9f11
Aripov-Sirojiddin <aripov.2@osu.edu>
2024-08-05 14:54:47 -0400

    Setting up user show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get "users/new" => "users#new"
   get "about" => "about#index"
+  get "users/:id" => "users#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
