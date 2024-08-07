b9efdd905813daa6c060a9f6e7b2b2c3285dba58
Aripov-Sirojiddin <aripov.2@osu.edu>
2024-08-06 21:02:42 -0400

    Adding untracked file

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,80 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "valid user creation" do
+    get users_sign_up_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: {
+        user: {
+          name: "Valid user",
+          email: "valid-3949449@email.com",
+          username: "valid_username",
+          bio: "",
+          password: "valid_password",
+          password_confirmation: "valid_password",
+        }
+      }
+    end
+    follow_redirect!
+    assert flash[:success]
+    assert_not flash[:danger]
+    assert_template 'users/show'
+  end
+  test "taken email" do
+    get users_sign_up_path
+    assert_no_difference 'User.count' do
+      post users_path, params: {
+        user: {
+          name: "",
+          email: "jitewaboh@lagify.com",
+          username: "available_username",
+          bio: "",
+          password: "password",
+          password_confirmation: "password"
+        }
+      }
+    end
+    assert_template 'users/new'
+    assert_select 'div.alert-danger'
+    assert_select 'div#error_explanation'
+    assert flash[:danger]
+  end
+
+  test "passwords don't match" do
+    assert_no_difference 'User.count' do
+      post users_path, params: {
+        user: {
+          name: "",
+          email: "avaialeble_email_38558585932q@email.com",
+          username: "available_username",
+          bio: "",
+          password: "password",
+          password_confirmation: "doesn't match"
+        }
+      }
+    end
+    assert_template 'users/new'
+    assert_select 'div.alert-danger'
+    assert_select 'div#error_explanation'
+    assert flash[:danger]
+  end
+
+  test "username taken" do
+    assert_no_difference 'User.count' do
+      post users_path, params: {
+        user: {
+          name: "",
+          email: "jitewaboh@lagify.com",
+          username: "example_account",
+          bio: "",
+          password: "password",
+          password_confirmation: "password"
+        }
+      }
+    end
+    assert_template 'users/new'
+    assert_select 'div.alert-danger'
+    assert_select 'div#error_explanation'
+    assert flash[:danger]
+  end
+end
