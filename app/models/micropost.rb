3e726f8019ebf331688231397e63fb23994729a5
Aripov-Sirojiddin <aripov.2@osu.edu>
2024-08-28 19:42:57 -0400

    Added ability to attach images

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140, message: "At least 5 characters and at most 140 characters is required!" }
